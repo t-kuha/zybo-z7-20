3786ae496012c12af6308bbf6538122bdc59fa2a
t_kuha <imagingtechnerd@gmail.com>
2020-09-16 11:42:26 +0900

    Update create_vivado_project.tcl

--- simple/create_vivado_project.tcl
+++ simple/create_vivado_project.tcl
@@ -3,21 +3,21 @@ set PRJ_DIR     _vivado
 set PRJ_NAME    z7_20
 set BD_NAME     ${PRJ_NAME}
 set SRC_DIR     src
-# set XSDK_DIR    sdk
 set NUM_JOBS    4
-set DSA_OUT_DIR petalinux
 
+# Remove existing directory
+file delete -force ${PRJ_DIR}
 
 # Create project
 create_project ${PRJ_NAME} ${PRJ_DIR} -part xc7z020clg400-1
 set_property board_part digilentinc.com:zybo-z7-20:part0:1.0 [current_project]
 
 # Create block design
-source $SRC_DIR/bd.tcl
+source ${SRC_DIR}/bd.tcl
 
 # Set top-level source
-make_wrapper -files [get_files ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${PRJ_NAME}/${BD_NAME}.bd] -top
-add_files -norecurse ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${PRJ_NAME}/hdl/${BD_NAME}_wrapper.v
+make_wrapper -files [get_files ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/${BD_NAME}.bd] -top
+add_files -norecurse ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/hdl/${BD_NAME}_wrapper.v
 set_property top ${BD_NAME}_wrapper [current_fileset]
 update_compile_order -fileset sources_1
 
@@ -25,12 +25,11 @@ update_compile_order -fileset sources_1
 regenerate_bd_layout
 # validate_bd_design
 save_bd_design
-generate_target all [get_files  ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${PRJ_NAME}/${PRJ_NAME}.bd]
+generate_target all [get_files  ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/${BD_NAME}.bd]
 
 # Create .dsa
-file mkdir ${DSA_OUT_DIR}
-write_dsa ${DSA_OUT_DIR}/${PRJ_NAME}.dsa
-validate_dsa ${DSA_OUT_DIR}/${PRJ_NAME}.dsa
+write_dsa -force ${PRJ_NAME}.dsa
+validate_dsa ${PRJ_NAME}.dsa
 
 # Finish - close project
 close_project
